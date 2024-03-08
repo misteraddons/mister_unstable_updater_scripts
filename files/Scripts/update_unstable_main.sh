560f90e887d9283fce4b979cb3c701f87d31266c
Robot <robot@M1MBP.localdomain>
2024-03-08 10:18:44 -0700

    Use theypsilon downloader script for unstable cores. Create separate unstable main updater
    Instructions:
    1) Place update_unstable.ini in main folder of sd card
    2) Place update_unstable.sh in Scripts folder
    3) Place update_unstable_main.sh in Scripts folder
    
    Note: unstable cores and unstable main binary are for bleeding edge testing and should not be used as the basis for bug reports  unless the core author is aware you're using nightly builds.

--- files/Scripts/update_unstable_main.sh
+++ files/Scripts/update_unstable_main.sh
@@ -7,17 +7,12 @@
 # Special thanks to wpo and revoice for suggestions and code snippets
 
 # URLs
-unstable_core_url="https://raw.githubusercontent.com/MiSTer-unstable-nightlies/Unstable_Folder_MiSTer/main/db_unstable_nightlies_folder.json"
 urlmain="https://api.github.com/repos/MiSTer-unstable-nightlies/Main_MiSTer/releases"
 
 # Directories
 tmp=$(mktemp -d)
 rootDir="/media/fat"
 tmp2="$rootDir/Scripts"
-dest="$rootDir/_Unstable"
-
-# Unstable build Manifest 
-unstableList="unstable.txt"
 
 # Function Definitions
 check_err(){ error_state=$(echo $?)
@@ -35,51 +30,6 @@ function test_internet_connection(){
 
 # Start
 test_internet_connection
-
-# Create target install path for unstable cores
-mkdir -p "$dest"
-check_err "[Fatal Error] unable to create target path for <$dest>"
-
-# Parse JSON endpoint for new unstable cores!
-wget -q -O- $unstable_core_url | jq -r '.files[] | .url | select(contains("rbf"))' > $tmp/$unstableList
-
-# Process downloaded files from manifest
-while read line;
-do
-  # Get basename of the download file
-  fn=`basename $line`
-  echo "filename " $fn
-  corename=${fn%_202*}
-  # echo "corename " $corename # Debug
-
-  # Check if newest core version already exists
-  if [ -f "$dest/$fn" ]; then
-    echo "[Ok] core '$fn' already the latest version"
-  else
-    echo "[Action] Update Found, Downloading '$fn' now"
-    # Attempt to download the file and organize
-    if wget -q -P $tmp $line; then
-        echo "[Action] Latest File Downloaded"
-        echo "[Action] Removing previous version"
-        rm $dest/$corename*
-        echo "[Action] Adding New version '$fn' to $dest"
-        mv $tmp/$fn $dest
-    else
-      echo "[Warning] Download Failed - Please Try Again Later"
-    fi
-  fi
-done < $tmp/$unstableList
-
-echo "[Ok] Unstable cores all updated! Have fun"
-
-rm -f $rootDir/_Unstable/Saturn_SingleSDRAM_unstable_20231004_09df08.rbf
-rm -f $rootDir/_Unstable/Saturn_DualSDRAM_unstable_20231004_09df08.rbf
-
-#exit
-
-####################################################################################################
-##### !!! REMOVE THE EXIT ABOVE THIS SECTION IF YOU WANT TO UPDATE TO MISTER MAIN UNSTABLE !!! #####
-####################################################################################################
 
 #Get latest unstable core link
 main=$(wget -q -O- $urlmain | jq -r '[.[0].assets[] | select(.name|startswith("MiSTer_unstable_202"))] | sort_by(.created_at)[-1:][] | .browser_download_url')
